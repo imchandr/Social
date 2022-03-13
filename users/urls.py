0a3876de8ded69a3d994d1d178745bdba2e32d22
Chandr <cksahuu@gmail.com>
2022-03-13 19:17:01 +0530

    fix mutual friends section

--- users/urls.py
+++ users/urls.py
@@ -5,12 +5,12 @@ from users import views as user_views
 
 app_name = 'users'
 urlpatterns = [
-    path('profile/',user_views.my_profile, name='profile'),
     path('', user_views.users_list, name='users_list'),
+    path('profile/',user_views.my_profile, name='profile'),
+	path('<int:phone>/', user_views.profile_view, name='profile_view'),
 	path('friends/', user_views.friend_list, name='friend_list'),
     path('friend-request/send/<int:id>/', user_views.send_friend_request, name='send_friend_request'),
     path('friend-request/accept/<int:id>/', user_views.accept_friend_request, name='accept_friend_request'),
     path('search_users/', user_views.search_users, name='search_users'),
     path('search/', user_views.search_api, name='search'),
-	path('<int:phone>/', user_views.profile_view, name='profile_view'),
 ]
--- users/views.py
+++ users/views.py
@@ -8,9 +8,8 @@ from account.models import User
 from django.conf import settings
 from django.http import HttpResponseRedirect, JsonResponse
 from users.models import Profile, FriendRequest
-from users.forms import  UserUpdateForm, ProfileUpdateForm
+from users.forms import UserUpdateForm, ProfileUpdateForm
 import random
-
 
 
 @login_required
@@ -47,142 +46,146 @@ def users_list(request):
     }
     return render(request, "users/users_list.html", context)
 
+
 def friend_list(request):
-	p = request.user.profile
-	print(p)
-	friends = p.friends.all()
-	context={
-	'friends': friends
-	}
-	return render(request, "users/profile.html", context)
+    p = request.user.profile
+    friends = p.friends.all()
+    context = {
+        'friends': friends
+    }
+    return render(request, "users/profile.html", context)
+
 
 @login_required
 def send_friend_request(request, id):
-	user = get_object_or_404(User, id=id)
-	frequest, created = FriendRequest.objects.get_or_create(
-			from_user=request.user,
-			to_user=user)
-	messages.success(request, 'Friend Request send successfully')
-	return HttpResponseRedirect('/users/')
+    user = get_object_or_404(User, id=id)
+    frequest, created = FriendRequest.objects.get_or_create(
+        from_user=request.user,
+        to_user=user)
+    messages.success(request, 'Friend Request send successfully')
+    return HttpResponseRedirect('/users/')
+
 
 @login_required
 def accept_friend_request(request, id):
-	from_user = get_object_or_404(User, id=id)
-	print('\n\n ', from_user)
-	frequest = FriendRequest.objects.filter(from_user=from_user, to_user=request.user).first()
-	user1 = frequest.to_user
-	user2 = from_user
-	user1.profile.friends.add(user2.profile)
-	user2.profile.friends.add(user1.profile)
-	if(FriendRequest.objects.filter(from_user=request.user, to_user=from_user).first()):
-		request_rev = FriendRequest.objects.filter(from_user=request.user, to_user=from_user).first()
-		request_rev.delete()
-	frequest.delete()
-	return HttpResponseRedirect('/users/{}'.format(request.user.profile.slug))
-
+    from_user = get_object_or_404(User, id=id)
+    frequest = FriendRequest.objects.filter(
+        from_user=from_user, to_user=request.user).first()
+    user1 = frequest.to_user
+    user2 = from_user
+    user1.profile.friends.add(user2.profile)
+    user2.profile.friends.add(user1.profile)
+    if(FriendRequest.objects.filter(from_user=request.user, to_user=from_user).first()):
+        request_rev = FriendRequest.objects.filter(
+            from_user=request.user, to_user=from_user).first()
+        request_rev.delete()
+    frequest.delete()
+    return HttpResponseRedirect('/users/{}'.format(request.user.profile.slug))
 
 
 @login_required
 def profile_view(request, phone):
-	p = Profile.objects.filter(slug=phone).first()
-	u = p.user
-	sent_friend_requests = FriendRequest.objects.filter(from_user=p.user)
-	rec_friend_requests = FriendRequest.objects.filter(to_user=p.user)
-	user_posts = Post.objects.filter(author=u)
+    p = Profile.objects.filter(slug=phone).first()
+    u = p.user
+    sent_friend_requests = FriendRequest.objects.filter(from_user=p.user)
+    rec_friend_requests = FriendRequest.objects.filter(to_user=p.user)
+    user_posts = Post.objects.filter(author=u)
 
-	friends = p.friends.all()
+    friends_list = p.friends.exclude(user=request.user)
 
-	# is this user our friend
-	button_status = 'none'
-	if p not in request.user.profile.friends.all():
-		button_status = 'not_friend'
+    # is this user our friend
+    button_status = 'none'
+    if p not in request.user.profile.friends.all():
+        button_status = 'not_friend'
 
-		# if we have sent him a friend request
-		if len(FriendRequest.objects.filter(
-			from_user=request.user).filter(to_user=p.user)) == 1:
-				button_status = 'friend_request_sent'
+        # if we have sent him a friend request
+        if len(FriendRequest.objects.filter(
+                from_user=request.user).filter(to_user=p.user)) == 1:
+            button_status = 'friend_request_sent'
 
-		# if we have recieved a friend request
-		if len(FriendRequest.objects.filter(
-			from_user=p.user).filter(to_user=request.user)) == 1:
-				button_status = 'friend_request_received'
+        # if we have recieved a friend request
+        if len(FriendRequest.objects.filter(
+                from_user=p.user).filter(to_user=request.user)) == 1:
+            button_status = 'friend_request_received'
 
-	context = {
-		'u': u,
-		'button_status': button_status,
-		'friends_list': friends,
-		'sent_friend_requests': sent_friend_requests,
-		'rec_friend_requests': rec_friend_requests,
-		'post_count': user_posts.count
-	}
+    context = {
+        'u': u,
+        'button_status': button_status,
+        'friends': friends_list,
+        'mfrnd_count': friends_list.count,
+        'sent_friend_requests': sent_friend_requests,
+        'rec_friend_requests': rec_friend_requests,
+        'post_count': user_posts.count
+    }
 
-	return render(request, "users/profile.html", context)
-
+    return render(request, "users/profile.html", context)
 
 
 @login_required
 def my_profile(request):
-	p = request.user.profile
-	you = p.user
-	sent_friend_requests = FriendRequest.objects.filter(from_user=you)
-	rec_friend_requests = FriendRequest.objects.filter(to_user=you)
-	user_posts = Post.objects.filter(author=you)
-	friends = p.friends.all()
+    p = request.user.profile
+    you = p.user
+    sent_friend_requests = FriendRequest.objects.filter(from_user=you)
+    rec_friend_requests = FriendRequest.objects.filter(to_user=you)
+    user_posts = Post.objects.filter(author=you)
+    friends = p.friends.all()
 
-	# is this user our friend
-	button_status = 'none'
-	if p not in request.user.profile.friends.all():
-		button_status = 'not_friend'
+    # is this user our friend
+    button_status = 'none'
+    if p not in request.user.profile.friends.all():
+        button_status = 'not_friend'
 
-		# if we have sent him a friend request
-		if len(FriendRequest.objects.filter(
-			from_user=request.user).filter(to_user=you)) == 1:
-				button_status = 'friend_request_sent'
+        # if we have sent him a friend request
+        if len(FriendRequest.objects.filter(
+                from_user=request.user).filter(to_user=you)) == 1:
+            button_status = 'friend_request_sent'
 
-		if len(FriendRequest.objects.filter(
-			from_user=p.user).filter(to_user=request.user)) == 1:
-				button_status = 'friend_request_received'
+        if len(FriendRequest.objects.filter(
+                from_user=p.user).filter(to_user=request.user)) == 1:
+            button_status = 'friend_request_received'
 
-	# List of friends
-	p = request.user.profile
-	friends = p.friends.all()
-	
-	context = {
-		'u': you,
-		'button_status': button_status,
-		'friends_list': friends,
-		'sent_friend_requests': sent_friend_requests,
-		'rec_friend_requests': rec_friend_requests,
-		'post_count': user_posts.count,
-		'friends': friends
-	}
+    # List of friends
+    p = request.user.profile
+    friends = p.friends.all()
 
-	return render(request, "users/profile.html", context)
+    context = {
+        'u': you,
+        'button_status': button_status,
+        'friends_list': friends,
+        'sent_friend_requests': sent_friend_requests,
+        'rec_friend_requests': rec_friend_requests,
+        'post_count': user_posts.count,
+        'friends': friends
+    }
+
+    return render(request, "users/profile.html", context)
+
 
 @login_required
 def search_users(request):
-	query = request.GET.get('q')
-	users_list = []
-	if query:
-		users_list = User.objects.filter(first_name__icontains=query) | User.objects.filter(last_name__icontains=query) | User.objects.filter(email__icontains=query) | User.objects.filter(phone__icontains=query)
+    query = request.GET.get('q')
+    friends_list = request.user.profile.friends.all()
+    print('\n\n', friends_list)
+    users_list = []
+    if query:
+        users_list = User.objects.filter(first_name__icontains=query) | User.objects.filter(
+            last_name__icontains=query) | User.objects.filter(email__icontains=query) | User.objects.filter(phone__icontains=query)
 
-	
-		
-	return render(request, "users/search.html", {'users': users_list})
-    
+    return render(request, "users/search.html", {'users': users_list, 'friends': friends_list})
+
+
 def search_api(request):
     query = request.GET.get('q')
     payload = []
-    
+
     if query:
-        q_results = User.objects.filter(first_name__icontains=query)|User.objects.filter(last_name__icontains=query)|User.objects.filter(email__icontains=query)|User.objects.filter(phone__icontains=query)
-        
+        q_results = User.objects.filter(first_name__icontains=query) | User.objects.filter(
+            last_name__icontains=query) | User.objects.filter(email__icontains=query) | User.objects.filter(phone__icontains=query)
+
         for result in q_results:
             payload.append(result.first_name)
             payload.append(result.last_name)
             payload.append(result.email)
             payload.append(result.phone)
-            
-    return JsonResponse({'status':200, 'data':payload})
 
-     
+    return JsonResponse({'status': 200, 'data': payload})
